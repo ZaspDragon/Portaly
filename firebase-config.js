ba9f7fd4ed5b31ff6fa87b39a846501e95c11544
ZaspDragon <ilevanshine@gmail.com>
2026-05-09 15:35:40 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+// Portaly Firebase Config
+// This file is safe to use on the frontend.
+// Do NOT place Stripe secret keys, Firebase admin keys, or private server keys here.
+
+window.PORTALY_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDfF4mmLeI4IbOl3TsWJnCfMg_nsSfqTp0",
+  authDomain: "portaly-d6617.firebaseapp.com",
+  projectId: "portaly-d6617",
+  storageBucket: "portaly-d6617.firebasestorage.app",
+  messagingSenderId: "594971277057",
+  appId: "1:594971277057:web:8a0f6c29370d741a45cd00",
+  measurementId: "G-L8EHWRS2BZ"
+};

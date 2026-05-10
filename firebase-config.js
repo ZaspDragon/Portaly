fdfb34bd4fe0f36400756966d85491380cc4231d
ZaspDragon <ilevanshine@gmail.com>
2026-05-09 20:35:12 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,11 @@
 
   Safe for the browser:
   - Firebase web app config
-  - Stripe publishable key placeholder
+  - Public app URL
   - Public backend URL placeholder
 
-  Never place Stripe secret keys, webhook secrets, or Firebase Admin credentials here.
+  Never place Square access tokens, webhook secrets, Stripe secret keys,
+  Firebase Admin credentials, or private server keys here.
 */
 
 window.PORTALY_FIREBASE_CONFIG = {
@@ -14,26 +15,14 @@ window.PORTALY_FIREBASE_CONFIG = {
   trialDays: 14,
   appUrl: "https://zaspdragon.github.io/Portaly/",
   functionsBaseUrl: "",
-  stripePublishableKey: "",
-  stripePriceIds: {
-    starter: "",
-    agency: "",
-    growth: ""
-  },
+
+  billingProvider: "square",
+
   apiKey: "AIzaSyDfF4mmLeI4IbOl3TsWJnCfMg_nsSfqTp0",
   authDomain: "portaly-d6617.firebaseapp.com",
   projectId: "portaly-d6617",
   storageBucket: "portaly-d6617.firebasestorage.app",
   messagingSenderId: "594971277057",
   appId: "1:594971277057:web:8a0f6c29370d741a45cd00",
-  measurementId: "G-L8EHWRS2BZ",
-  firebaseConfig: {
-    apiKey: "AIzaSyDfF4mmLeI4IbOl3TsWJnCfMg_nsSfqTp0",
-    authDomain: "portaly-d6617.firebaseapp.com",
-    projectId: "portaly-d6617",
-    storageBucket: "portaly-d6617.firebasestorage.app",
-    messagingSenderId: "594971277057",
-    appId: "1:594971277057:web:8a0f6c29370d741a45cd00",
-    measurementId: "G-L8EHWRS2BZ"
-  }
+  measurementId: "G-L8EHWRS2BZ"
 };

2a3bbaad39587758f1514bea29f055f9f515e5ca
ZaspDragon <ilevanshine@gmail.com>
2026-06-24 18:02:30 -0400

    Update billing-config.js

--- billing-config.js
+++ billing-config.js
@@ -1,8 +1,8 @@
 window.PORTALY_BILLING_CONFIG = {
   provider: "square",
   functionsBaseUrl: "",
-  starterPaymentLink: "https://square.link/u/mfu6eun7",
-  agencyPaymentLink: "https://square.link/u/ojz2a1Au",
-  growthPaymentLink: "https://square.link/u/Iy99LyYg",
-  enterprisePaymentLink: "https://square.link/u/96br6x5W"
+  starterPaymentLink: "https://square.link/u/oujLXZJi",
+  agencyPaymentLink: "https://square.link/u/J3W2uQaN",
+  growthPaymentLink: "https://square.link/u/9QKb7z1C",
+  enterprisePaymentLink: "https://square.link/u/fvpEWC6H"
 };
